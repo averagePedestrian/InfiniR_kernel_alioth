65598b8156cfebab7037b51059faf94f079b1a28
averagePedestrian <nehalchoudhury.16@gmail.com>
2024-11-05 08:08:37 +0000

    Fix can_umount

--- fs/namespace.c
+++ fs/namespace.c
@@ -1645,10 +1645,12 @@ static inline bool may_mandlock(void)
 }
 #endif
 
-static inline int can_umount(const struct path *path, int flags)
+static int can_umount(const struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
 
+	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
+		return -EINVAL;
 	if (!may_mount())
 		return -EPERM;
 	if (path->dentry != path->mnt->mnt_root)
@@ -1662,7 +1664,6 @@ static inline int can_umount(const struct path *path, int flags)
 	return 0;
 }
 
-// caller is responsible for flags being sane
 int path_umount(struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
